c2c54ac178a6454c887a0e375701205e3697356f
Peter Giordano <pgg505@york.ac.uk>
2017-07-20 12:46:10 +0100

    20/07/17

--- Intern/main.rb
+++ Intern/main.rb
@@ -1,14 +1,10 @@
 #!/usr/bin/env ruby
 
-require "./analyser.rb"
+require "./uploader.rb"
 
-# uploader = Uploader.new()
-# fold1 = uploader.upload_folder("First", hex_colour: "#00FF00")
-# fold2 = uploader.upload_folder("Second", location: fold1)
-# change = uploader.update_file_permission(fold1, "pgg505@york.ac.uk")
-# file1 = uploader.upload_file("test.pdf", "A", fold2)
-# file2 = uploader.upload_file("flower.jpeg", "B", fold1)
-# text = uploader.upload_file("store.rb", "Test")
-
-analyser = Analyser.new()
-analyser.temp()
+begin
+  @uploader = Uploader.new()
+  @uploader.upload_filesystem
+rescue => error
+  p error
+end
--- Intern/uploader.rb
+++ Intern/uploader.rb
@@ -3,98 +3,106 @@ require "json"
 require "filemagic"
 require "./authoriser.rb"
 
-class Uploader # Uploads files and folders to an authorised Google Drive account.
-  def initialize()
-    authoriser = Authoriser.new()
-    authoriser.authorise() # prompts user to authorise their account
-    @access_token = authoriser.get_access_token() # saves generated access token
-    @drive_uploader = RestClient::Resource.new("https://www.googleapis.com/drive/v3/files/", # used for all requests except file upload
+class Uploader # uploads files and folders to an authorised Google Drive account
+  def initialize # prompts user to authorise a Google Drive account and creates resources
+    authoriser = Authoriser.new
+    authoriser.authorise # prompts user to authorise their account
+    @access_token = authoriser.get_access_token # saves generated access token
+    @drive_manager = RestClient::Resource.new("https://www.googleapis.com/drive/v3/files/",
+                                              :headers => {"Authorization" => "Bearer #{@access_token}",
+                                                           "Content-Type" => "application/json"})
+    @drive_uploader = RestClient::Resource.new("https://www.googleapis.com/upload/drive/v3/files",
                                                :headers => {"Authorization" => "Bearer #{@access_token}"})
+    @file_analyser = FileMagic.new(FileMagic::MAGIC_MIME)
   end
 
-  def upload_folder(folder_name, hex_colour: "#FFFF00", location: "root")
+  def upload_filesystem(upload_dest: "root", current_dir: "") # recursively uploads a filesystem
+    Dir[current_dir + "*"].each do |object| # does not return hidden objects
+      if object.include?(".") # checks if object is a file
+        file_name = object.split("/")[-1]
+        file_id = upload_file(object, file_name, location: upload_dest)
+      else
+        folder_name = object.split("/")[-1]
+        folder_id = upload_folder(folder_name, location: upload_dest)
+        upload_filesystem(upload_dest: folder_id, current_dir: current_dir + folder_name + "/")
+      end
+    end
+  end
+
+  def upload_folder(folder_name, hex_colour: "#A2FF33", location: "root") # uploads a folder with optional colour
     begin
-      upload = @drive_uploader.post(
+      upload = @drive_manager.post(
         {"name" => folder_name,
          "mimeType" => "application/vnd.google-apps.folder",
-         "folderColorRgb" => hex_colour}.to_json(),
-        {"Content-Type" => "application/json"})
+         "folderColorRgb" => hex_colour}.to_json)
     rescue => error
-        p error
+      p error
     end
 
-    parsed_output = JSON.parse(upload)
-    id = parsed_output["id"]
+    folder_id = JSON.parse(upload)["id"]
 
-    if location != "root" then # does not need to change parents if intended location is root
+    if location != "root" # does not need to change parents if intended location is root
       begin
-        move = @drive_uploader[id + "?addParents=" + location + "&removeParents=root&alt=json"].patch(
-          {"uploadType" => "resumable"}.to_json(),
-          {"Content-Type" => "application/json"})
+        move = @drive_manager[folder_id + "?addParents=" + location + "&removeParents=root&alt=json"].patch(
+          {"uploadType" => "resumable"}.to_json)
       rescue => error
         p error
       end
     end
 
-    return id
+    return folder_id
   end
 
-  def update_file_permission(file_id, email)
-    payload = {"role" => "writer",
-               "type" => "group",
-               "emailAddress" => email}.to_json()
-
+  def upload_file(file_path, file_name, location: "root")
     begin
-      update = @drive_uploader[file_id + "/permissions"].post(
-        payload,
-        {"Content-Type" => "application/json"})
-    rescue => error
-      p error
-    end
-
-    return update
-  end
-
-  def upload_file(file_path, file_name, folder_id: "root")
-    '''(String, String, String) -> String. Uploads a given file to the specified folder and returns its generated file ID.'''
-
-    begin
-      file_type = FileMagic.new(FileMagic::MAGIC_MIME).file(file_path).split(";")[0]
+      file_type = @file_analyser.file(file_path).split(";")[0]
       payload = File.open(file_path)
     rescue => error
       p error
     end
 
-    if file_type == "text/plain" then # uploads as JSON which is problematic
+    if file_type == "text/plain"  # uploads as json which is not ideal
       begin
-        upload = RestClient.post( # uploads file
-          "https://www.googleapis.com/upload/drive/v3/files",
+        upload = @drive_uploader.post(
           payload,
-          {"Authorization" => "Bearer #{@access_token}", "Content-Type" => "application/json"})
+          {"Content-Type" => "application/json"})
       rescue => error
           p error
       end
-    else
+    else # uploads complex files properly
       begin
-        upload = RestClient.post( # uploads file
-          "https://www.googleapis.com/upload/drive/v3/files",
-          payload,
-          {"Authorization" => "Bearer #{@access_token}"})
+        upload = @drive_uploader.post(
+          payload)
       rescue => error
           p error
       end
     end
 
-    parsed_output = JSON.parse(upload)
-    id = parsed_output["id"]
+    file_id = JSON.parse(upload)["id"]
 
     begin
-      rename = @drive_uploader[id + "?addParents=" + folder_id + "&removeParents=root&alt=json"].patch( # changes file name and puts in folder
-        {"uploadType" => "resumable", "name" => file_name}.to_json(),
-        {"Content-Type" => "application/json"})
+      rename = @drive_manager[file_id + "?addParents=" + location + "&removeParents=root&alt=json"].patch( # changes file name and puts in folder
+        {"uploadType" => "resumable",
+         "name" => file_name}.to_json)
     rescue => error
       p error
     end
-    return id
+
+    return file_id
+  end
+
+  def update_file_permission(file_id, email)
+    payload = {"role" => "writer",
+               "type" => "group",
+               "emailAddress" => email}.to_json
+
+    begin
+      update = @drive_manager[file_id + "/permissions"].post(
+        payload)
+    rescue => error
+      p error
+    end
+
+    return update
   end
 end
